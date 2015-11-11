219336862260ac51e8c550b1c8f485fcc0d62794
heimusu <heimusu@gmail.com>
2015-11-12 00:03:55 +0900

    DB接続と参照まで

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ connection.connect(function(err){
         console.log('Error connecting to DB');
         return ;
     }
+
+    console.log('Success connecting to DB');
 });
+
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -22,9 +25,17 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
+    connection.query('SELECT * FROM accounts', function(err, rows){
+        if(err){
+            throw err;
+        }
+        console.log('Data received from Db:¥n');
+        console.log(rows);
+        var user_name = rows[0].user_name;
+        //res.render('index', {title: 'Express Sample', userData: rows});
+        res.render('index', {userData: rows});
+    });
     res.render('index', {title: 'Express Sample'});
 });
 
 app.listen('3000');
-
-
